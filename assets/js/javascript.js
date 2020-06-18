c03632a1bd31f9a9c905bf7ea6fd878a1da9b859
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-06-18 01:23:25 -0500

    refractored function into two

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,22 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     //we're trying to select an HTML element by one of its attributes.
     //In this case, we're selecting the <input> element on the page that has a name attribute set to a value of "task-name".
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
     // creates list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -15,11 +25,12 @@ var createTaskHandler = function(event) {
     // give the div a class name
     taskInfoEl.className = "task-info";
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
     
     tasksToDoEl.appendChild(listItemEl);
-};
+}
+
 // this has to stay below it's function otherwise it'll come back undefined (top down reading)
-formEl.addEventListener("submit", createTaskHandler); 
+formEl.addEventListener("submit", taskFormHandler); 
