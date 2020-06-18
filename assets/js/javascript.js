c9a988438161ba7a90825a99055e87ef8fd550d6
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-06-17 19:04:42 -0500

    input fields are submitting. need to refractor function

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,9 +3,22 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    //we're trying to select an HTML element by one of its attributes.
+    //In this case, we're selecting the <input> element on the page that has a name attribute set to a value of "task-name".
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // creates list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give the div a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
 };
 // this has to stay below it's function otherwise it'll come back undefined (top down reading)
