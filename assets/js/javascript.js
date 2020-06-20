40232202f1b5a40b8a1179151da51981c624aa32
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-06-19 22:41:01 -0500

    cleaned up drag and drop GUI

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -193,6 +193,7 @@ var dropZoneDragHandler = function (event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 
 };
@@ -217,8 +218,16 @@ var dropTaskHandler = function (event) {
     else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 // this has to stay below it's function otherwise it'll come back undefined (top down reading)
 formEl.addEventListener("submit", taskFormHandler);
@@ -227,3 +236,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
